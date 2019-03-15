136e01d1db2d2bcc4cfe19c0b687fd8cdc86d6e3
fellipeotavionunes <pipito321@outlook.com>
2019-03-15 15:25:40 -0300

    Passo 01

---
+++ 123atividade 03/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _123atividade_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+      
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.BackgroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine("                                     =====================================");
+            Console.WriteLine("                                     Fellipe Otavio Nunes TI21 Atividae_03");
+            Console.WriteLine("                                     =====================================");
+            Console.ReadKey();
+            
+        }
+    }
+}
