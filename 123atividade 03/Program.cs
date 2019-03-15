f996162e868791b48fe40296882480ece14ee31a
fellipeotavionunes <pipito321@outlook.com>
2019-03-15 16:31:22 -0300

    Nada Mudando de Cor

--- 123atividade 03/Program.cs
+++ 123atividade 03/Program.cs
@@ -10,14 +10,67 @@ namespace _123atividade_03
     {
         static void Main(string[] args)
         {
-      
+
             Console.ForegroundColor = ConsoleColor.Cyan;
             Console.BackgroundColor = ConsoleColor.DarkRed;
-            Console.WriteLine("                                     =====================================");
-            Console.WriteLine("                                     Fellipe Otavio Nunes TI21 Atividae_03");
-            Console.WriteLine("                                     =====================================");
+            Console.WriteLine("                                     ===================================== ");
+            Console.WriteLine("                                     Fellipe Otavio Nunes TI21 Atividae_03 ");
+            Console.WriteLine("                                     ===================================== ");
             Console.ReadKey();
-            
+            Console.WriteLine("Commit 02");
+            Console.ResetColor();
+
+
+            int i=1;
+            while (i<6000) {
+                i++;
+                Console.Clear();
+                Console.WriteLine(i);
+                Console.WriteLine("");
+                Console.WriteLine("");
+                Console.WriteLine("");
+                Console.WriteLine("");
+                Console.WriteLine("");
+                Console.WriteLine("");
+                Console.WriteLine("");
+                Console.WriteLine("");
+                Console.WriteLine("");
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.WriteLine("                NADA MUDANDO DE COR");
+                Console.Clear();
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.WriteLine("                NADA MUDANDO DE COR");
+                Console.Clear();
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("                NADA MUDANDO DE COR");
+                Console.Clear();
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("                NADA MUDANDO DE COR");
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("                NADA MUDANDO DE COR");
+                Console.Clear();
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("                NADA MUDANDO DE COR");
+                Console.Clear();
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                Console.WriteLine("                NADA MUDANDO DE COR");
+                Console.Clear();
+                Console.ForegroundColor = ConsoleColor.Gray;
+                Console.WriteLine("                NADA MUDANDO DE COR");
+                Console.WriteLine("");
+                Console.WriteLine("");
+                Console.WriteLine("");
+                Console.WriteLine("");
+                Console.WriteLine("");
+                Console.WriteLine("");
+                Console.WriteLine("");
+                Console.WriteLine("");
+                Console.WriteLine("");
+                Console.WriteLine("");
+            }
+            Console.ReadKey();
+
+
         }
     }
 }
